6f564586ba2bd9c74c7fd64908328c72799c8ed9
suwei <910933955@qq.com>
2021-08-13 09:07:19 +0800

    fix: fixCommit

---
+++ src/views/home/components/TopContainer.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <div>ASDF</div>
+    <el-card shadow="always" :body-style="{ padding: '20px' }">
+      <div slot="header">
+        <span>this is title</span>
+      </div>
+      this is body
+    </el-card>
+    <el-card shadow="always" :body-style="{ padding: '20px' }">
+      <div slot="header">
+        <span>this is title</span>
+      </div>
+      this is body
+    </el-card>
+    <el-card shadow="always" :body-style="{ padding: '20px' }">
+      <div slot="header">
+        <span>this is title</span>
+      </div>
+      this is body
+    </el-card>
+    <el-card shadow="always" :body-style="{ padding: '20px' }">
+      <div slot="header">
+        <span>this is title</span>
+      </div>
+      this is body
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TopContainer",
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/views/home/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <top-container />
+  </div>
+</template>
+
+<script>
+import TopContainer from './components/TopContainer.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    TopContainer
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
