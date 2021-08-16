cb3629dd23f70e39e50e194953a5d30080037203
suwei <william.su@zenlayer.com>
2021-08-16 17:01:53 +0800

    fix: 图表显示

--- src/main.js
+++ src/main.js
@@ -2,10 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
-import VueECharts from 'vue-echarts'
 
 Vue.config.productionTip = false
-Vue.component('v-chart',VueECharts)
 
 new Vue({
   router,
--- src/views/bMap/index.vue
+++ src/views/bMap/index.vue
@@ -1,39 +1,86 @@
 <template>
-  <div>
-    <v-chart class="chart" :option="option"/>
-  </div>
+  <v-chart class="chart" :option="option" />
 </template>
 
 <script>
-  export default {
-    name:'BMap',
-    data() {
-      return {
-        option:{
+import { use } from "echarts/core";
+import { CanvasRenderer } from "echarts/renderers";
+import { PieChart } from "echarts/charts";
+import {
+  TitleComponent,
+  TooltipComponent,
+  LegendComponent
+} from "echarts/components";
+import VChart, { THEME_KEY } from "vue-echarts";
 
-              xAxis: {
-        type: 'category',
-        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-    },
+use([
+  CanvasRenderer,
+  PieChart,
+  TitleComponent,
+  TooltipComponent,
+  LegendComponent
+]);
 
-
-    
-    yAxis: {
-        type: 'value'
-    },
-    series: [{
-        data: [150, 230, 224, 218, 135, 147, 260],
-        type: 'line'
-    }]
-        }
+export default {
+  name: "HelloWorld",
+  components: {
+    VChart
+  },
+  provide: {
+    [THEME_KEY]: "dark"
+  },
+  data() {
+    return {
+      option: {
+        title: {
+          text: "Traffic Sources",
+          left: "center"
+        },
+        tooltip: {
+          trigger: "item",
+          formatter: "{a} <br/>{b} : {c} ({d}%)"
+        },
+        legend: {
+          orient: "vertical",
+          left: "left",
+          data: [
+            "Direct",
+            "Email",
+            "Ad Networks",
+            "Video Ads",
+            "Search Engines"
+          ]
+        },
+        series: [
+          {
+            name: "Traffic Sources",
+            type: "pie",
+            radius: "55%",
+            center: ["50%", "60%"],
+            data: [
+              { value: 335, name: "Direct" },
+              { value: 310, name: "Email" },
+              { value: 234, name: "Ad Networks" },
+              { value: 135, name: "Video Ads" },
+              { value: 1548, name: "Search Engines" }
+            ],
+            emphasis: {
+              itemStyle: {
+                shadowBlur: 10,
+                shadowOffsetX: 0,
+                shadowColor: "rgba(0, 0, 0, 0.5)"
+              }
+            }
+          }
+        ]
       }
-    },
+    };
   }
+};
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
 .chart {
-  width: 400px;
   height: 400px;
 }
 </style>
